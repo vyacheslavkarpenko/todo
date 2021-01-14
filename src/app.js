0cbdf326e2b4bac5c00ebb24824c64397d60f288
vyacheslavkarpenko <viacheslavkarpenkoju@gmail.com>
2021-01-14 17:34:29 +0200

    Test Added test for route and some config.

--- src/app.js
+++ src/app.js
@@ -6,3 +6,4 @@ const tasks = require('./routes/tasks')
 app.use('/tasks', tasks)
 
 app.listen(3000);
+module.exports = app;
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -3,8 +3,8 @@ const router = express.Router()
 
 // all tasks
 router.get('/', function (req, res) {
-  res.send('<h1>Tasks</h1>')
+  console.log('tasks')
+  res.status(200).send("Hello World!");
 })
 
-console.log('tasks')
 module.exports = router
